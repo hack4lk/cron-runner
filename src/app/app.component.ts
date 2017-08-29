af116a716ec19f7e846da08e629ff51244bba96a
Lukasz Karpuk <lkarpuk@karpukcreative.com>
2017-08-28 20:55:37 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent{
+  seconds: number = 0;
+  url: string;
+  responseString: string;
+  timer;
+  running: boolean = false;
+  public startStop = "Start";
+  public output = [];
+  finished: boolean;
+  config;
+  getUrl;
+     
+  constructor(public http: HttpClient) {
+    http.get('./assets/app-config.json').subscribe(
+        (result: any) => {
+            //this.getUrl = result.geturl;
+            this.getUrl = result.get;  
+        }
+    );
+  }
+    
+  toggleInterval() {
+      if (!this.running) {
+          this.timer = setInterval( () => {
+              
+              this.http.get(this.getUrl + this.url, {responseType: 'text'}).subscribe(
+                (result) => {
+                    if (result.indexOf(this.responseString) === -1) {
+                        this.output.push(result);
+                        this.resetUI();
+                    }else{
+                        this.output.push(result);
+                    }
+                },
+                (error) => {
+                   this.output.push(error.message);
+                   this.resetUI();
+                }
+              );
+              
+          }, (this.seconds*1000) );
+          this.startStop = "Stop";
+      }else{
+          this.startStop = "Start";
+          clearInterval(this.timer);
+      }
+      this.running = !this.running;
+  }
+    
+  resetUI() {
+      this.startStop = "Start";
+      clearInterval(this.timer);
+      this.finished = true;
+  }
 }
